5affdc3d4b440f05db850347cb1d84dadde14153
Ruslan Kutaraev <ruslik0550@gmail.com>
2023-12-02 11:51:43 +0300

    code refactoring

---
+++ practice1/Main.java
@@ -0,0 +1,11 @@
+package practice1;
+
+public class Main {
+    public static void main(String[] args) {
+        Numbers numbers = new Numbers();
+        numbers.randomNumberList();
+        numbers.maxNumberList();
+        numbers.sumNumberListByCondition();
+        numbers.findNumberOfNumbersByCondition();
+    }
+}
