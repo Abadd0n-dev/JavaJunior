3757beef0d237dce96daa3f7401cf4c9acadee86
Ruslan Kutaraev <ruslik0550@gmail.com>
2023-12-23 12:17:00 +0300

    code refactoring 4

---
+++ practice4/Main/Main.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        String searchAuthor = "Роберт Мартин";
+        
+        // JDBC
+        System.out.println("JDBC");
+        DatabaseJDBC databaseJDBC = new DatabaseJDBC();
+        Connection connectionDatabase = databaseJDBC.dbConnection();
+        databaseJDBC.prepareTables(connectionDatabase);
+        databaseJDBC.insertData(connectionDatabase);
+        databaseJDBC.getData(connectionDatabase, searchAuthor);
+        databaseJDBC.dbClose(connectionDatabase);
+
+        // HIBERNATE
+        System.out.println("\nHIBERNATE");
+        DatabaseHibernate databaseHibernate = new DatabaseHibernate();
+        databaseHibernate.createTableBook();
+        databaseHibernate.getBooksByAuthor(searchAuthor);
+        databaseHibernate.closedSession();
+    }
+}
